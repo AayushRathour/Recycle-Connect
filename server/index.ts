c922746328d7375902aee2d59060a3a541cd4229
Aayush Rathour <aayushrathour34@gmail.com>
2026-02-01 23:24:05 +0530

    Fix database initialization - auto-create tables on startup

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,7 @@ import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
 import path from "path";
+import { initializeDatabase } from "./init-db";
 
 const app = express();
 const httpServer = createServer(app);
@@ -64,6 +65,9 @@ app.use((req, res, next) => {
 });
 
 (async () => {
+  // Initialize database tables on startup
+  await initializeDatabase();
+  
   await registerRoutes(httpServer, app);
 
   app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
